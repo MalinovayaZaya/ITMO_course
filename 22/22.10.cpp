a105c707331bf0c8a154aabc79345e131e42b6e7
Polina <32522035+MalinovayaZaya@users.noreply.github.com>
2019-02-26 22:11:41 +0300

    Update 22.10.cpp

--- 22/22.10.cpp
+++ 22/22.10.cpp
@@ -2,21 +2,11 @@
 #include <cmath>
 using namespace std;
 
-int MaxMin(int m, int string)
+void Matrix ( int** arr, int n, int m)
 {
-		int max = 0;
-	
-	for (int i = 0; i < m; i ++)
-		max += arr[string][i];
-		
-		returm max;
-}
-
-void Matrix ( int** arr, n, m)
-{
-	int max, min, temp, temp_string_max = 0, temp_string_min = 0;
+	int max, min, temp_string_max = 0, temp_string_min = 0;
 	max = 0;
-	
+	int max1 = 0;
 	for (int i = 0; i < m; i ++)
 		max += arr[0][i];
 		min = max;
@@ -25,24 +15,49 @@ void Matrix ( int** arr, n, m)
 	{
 		for (int j = 0; j < m; j ++)
 		{
-			temp = MaxMin(m, i);
-			if (max < temp)
+					
+			for (int r = 0; r < m; r ++)
+				max1 += arr[i][r];
+			
+			if (max < max1)
 			{
-				max = temp;
-				temp_string_max = 0
+				max = max1;
+				temp_string_max = i;
 			}
-			else if (min > temp){
+			else if (min > max1){
 				temp_string_min = i;
-				min = temt;
-				
+				min = max1;
 			}
 		}
 	}
+	cout << "The biggest string: ";
+	for (int q = 0; q < m; q++)
+	{
+		cout << arr[temp_string_max][q] << " ";	
+	}
+	cout << endl;
+	cout << "Summ of elements in the biggest string: " << max << endl;
+	
+	cout << "The lest string: ";
+	for (int q = 0; q < m; q++)
+	{
+		cout << arr[temp_string_min][q] << " ";	
+	}
+	cout << endl;
+	cout << "Summ of elements in the lest string: " << min << endl;
+	
+	
 }
 
 int main()
 {
-    int n, m;
+    int n;
+    int m;
     cin >> n >> m;
+    int a[n][m];
+    for (int i = 0; i < n; i++)
+    	for (int j = 0; j < m; j++)
+    		cin >> a[i][j];
+    Matrix(a, n, m);
     return 0;
 }
