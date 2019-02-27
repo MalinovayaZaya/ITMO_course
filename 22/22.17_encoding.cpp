076d068efb79feed31c60b5c85a5e38751a2518c
Polina <32522035+MalinovayaZaya@users.noreply.github.com>
2019-02-27 16:23:01 +0300

    Update 22.17_encoding.cpp

--- 22/22.17_encoding.cpp
+++ 22/22.17_encoding.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+using namespace std;
 
+int main() {
+	long long int X, Y, R;
+    ifstream fin("input.txt");
+if(fin.is_open())
+{
+    if(!fin.eof())
+    {
+     fin >> X >> Y;
+    }
+    fin.close(); 
+}
+else
+{
+    cout << "Fine not found";
+}
+ofstream fout("output.txt");
+{
+    if (- pow(2, 32) < X + Y && X + Y < pow(2, 32)) fout << X + Y; //"OVERFLOW" - в кодировке Win-1251 (сайт с кодировками - "https://wiki.iarduino.ru/page/encoding-arduino/")
+    else fout << "\117\126\105\122\106\114\117\127";
+}
+fout.close();
+	return 0;
+}
